e8785898fa01aa34896f1848f9bf400b52b4470a
vitalygrishin27 <vitalygrishin27@gmail.com>
2026-05-26 19:14:29 +0200

    New design

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = () => {
-   const MAIN_ENDPOINT = "http://173.242.57.145:8888/";
-   // const MAIN_ENDPOINT = "/api";
+   //const MAIN_ENDPOINT = "http://173.242.57.145:8888/";
+   const MAIN_ENDPOINT = "/api";
    //const MAIN_ENDPOINT = "http://localhost:8888/";
    //const MAIN_ENDPOINT = "http://192.168.0.213:8080/";
 
